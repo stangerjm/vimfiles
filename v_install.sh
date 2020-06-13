56bf66c123a5cbd64a35ca2c4cbda76ef8d03a48
stangerjm <jastange@amazon.com>
2020-06-13 00:10:49 +0000

    Added some conditionals to check to see if Neovim is installed, if Plug for Vim is installed, etc

--- v_install.sh
+++ v_install.sh
@@ -1,16 +1,29 @@
-# Install NeoVim because it's awesome!
-sudo mkdir -m 0755 /nix && sudo chown $USER /nix
-curl https://nixos.org/nix/install | sh
-source $HOME/.nix-profile/etc/profile.d/nix.sh
-nix-env -i neovim
+if which node > /dev/null
+then
+  if which nvim > /dev/null
+  then
+    echo "NeoVim is already installed, skipping installation"
+  else
+    # Install NeoVim because it's awesome!
+    sudo mkdir -m 0755 /nix && sudo chown $USER /nix
+    curl https://nixos.org/nix/install | sh
+    source $HOME/.nix-profile/etc/profile.d/nix.sh
+    nix-env -i neovim
+  fi
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  if [ ! -f ~/.vim/autoload/plug.vim ]
+  then
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  fi
 
-sudo npm -g install typescript
-sudo npm -g install neovim
+  sudo npm -g install typescript
+  sudo npm -g install neovim
 
-mkdir ~/.config
+  [ ! -d ~/.config ] && mkdir ~/.config
 
-cp -R ./nvim ~/.config
+  cp -R ./nvim ~/.config
 
-nvim +PlugInstall +qall
+  nvim +PlugInstall +qall
+else
+  echo "Node is not installed. Please install Node"
+fi
